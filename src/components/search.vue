6ade8275986de1686506a076a8ec56e74fba39a4
ArminWindlin <armin.windlin@gmail.com>
2021-04-05 22:46:39 +0200

    run search function on input

--- src/components/search.vue
+++ src/components/search.vue
@@ -1,34 +1,55 @@
 <template>
     <div class="search">
-        <input placeholder="search" />
+        <input placeholder="search" v-model="searchKey" @input="search"/>
+        <div class="results" v-if="results.length">
+            <div v-for="result in results">
+                {{ result.name }}
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-import News from '../data/home-articles.json';
 
 export default {
     name: 'search',
-    components: {},
     data() {
         return {
-            news: []
+            articles: [],
+            results: [],
+            searchKey: '',
         };
     },
+    methods: {
+        search() {
+            console.log(this.searchKey);
+        },
+    },
     beforeMount() {
-        this.news = News;
-    }
+        let category = 'fields';
+        let articleName = 'portal';
+        import(`../data/articles/${category}/${articleName}.json`).then((article) => {
+            this.articles.push = {
+                name: article.title,
+                text: JSON.stringify(article),
+            };
+        });
+    },
 };
 
 </script>
 
 <style scoped>
- input {
-     font-family: 'Muli', sans-serif;
-     text-align: left;
-     padding: 5px 20px;
-     font-size: 14px;
-     border-radius: 5px;
-     border: 1px solid #C4C4C4;
- }
+input {
+    font-family: 'Muli', sans-serif;
+    text-align: left;
+    padding: 5px 20px;
+    font-size: 14px;
+    border-radius: 5px;
+    border: 2px solid #C4C4C4;
+}
+
+.results {
+    background: white;
+}
 </style>
