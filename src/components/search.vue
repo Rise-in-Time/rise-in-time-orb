ebd6b2ff699afa2096a097294bc3d5db90788764
ArminWindlin <armin.windlin@gmail.com>
2021-04-05 22:33:48 +0200

    search setup

---
+++ src/components/search.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="search">
+        <input placeholder="search" />
+    </div>
+</template>
+
+<script>
+import News from '../data/home-articles.json';
+
+export default {
+    name: 'search',
+    components: {},
+    data() {
+        return {
+            news: []
+        };
+    },
+    beforeMount() {
+        this.news = News;
+    }
+};
+
+</script>
+
+<style scoped>
+ input {
+     font-family: 'Muli', sans-serif;
+     text-align: left;
+     padding: 5px 20px;
+     font-size: 14px;
+     border-radius: 5px;
+     border: 1px solid #C4C4C4;
+ }
+</style>
