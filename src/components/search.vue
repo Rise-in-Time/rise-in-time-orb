5cd95309a1d2cf1f4cdd6e22fd795a411e4eaf5c
ArminWindlin <armin.windlin@gmail.com>
2021-04-05 23:08:19 +0200

    search poc

--- src/components/search.vue
+++ src/components/search.vue
@@ -2,8 +2,9 @@
     <div class="search">
         <input placeholder="search" v-model="searchKey" @input="search"/>
         <div class="results" v-if="results.length">
-            <div v-for="result in results">
-                {{ result.name }}
+            <div class="result" v-for="result in results">
+                <div class="result-title">{{ result.name }}</div>
+                <p class="result-text">{{ result.displayText }}</p>
             </div>
         </div>
     </div>
@@ -22,24 +23,40 @@ export default {
     },
     methods: {
         search() {
-            console.log(this.searchKey);
+            this.results = [];
+            if (this.searchKey.length < 3) return;
+            this.articles.forEach(article => {
+                const position = article.text.search(this.searchKey);
+                if (position > -1) {
+                    const sliceStart = position - 16 > -1 ? position - 16 : 0;
+                    const sliceEnd = position + this.searchKey.length + 16 < article.text.length ?
+                            position + this.searchKey.length + 16 : article.text.length - 1;
+                    article.displayText = '...' + article.text.slice(sliceStart, sliceEnd) + '...';
+                    this.results.push(article);
+                }
+            });
+        },
+        importArticles() {
+            const category = 'fields';
+            const articleNames = ['portal', 'artifact', 'control', 'treasure'];
+            for (const articleName of articleNames) {
+                import(`../data/articles/${category}/${articleName}.json`).then((article) => {
+                    this.articles.push({
+                        name: article.title,
+                        text: JSON.stringify(article),
+                    });
+                });
+            }
         },
     },
     beforeMount() {
-        let category = 'fields';
-        let articleName = 'portal';
-        import(`../data/articles/${category}/${articleName}.json`).then((article) => {
-            this.articles.push = {
-                name: article.title,
-                text: JSON.stringify(article),
-            };
-        });
+        this.importArticles();
     },
 };
 
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 input {
     font-family: 'Muli', sans-serif;
     text-align: left;
@@ -51,5 +68,22 @@ input {
 
 .results {
     background: white;
+    padding: 10px 0;
+    width: 500px;
+    margin: 5px auto;
+    border-radius: 5px;
+
+    .result {
+        margin: 10px 0;
+
+        .result-title {
+            margin-bottom: 5px;
+        }
+
+        .result-text {
+            color: #6C6C6C;
+            line-height: 1.2em;
+        }
+    }
 }
 </style>
