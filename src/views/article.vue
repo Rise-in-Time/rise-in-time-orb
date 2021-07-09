55d1b030be655fe11cc38f7d0b6ee3ee25ebdc68
MrMichael3 <michael.kuechler2@uzh.ch>
2021-07-09 16:58:26 +0200

    change resurrection time from seconds to hours

--- src/views/article.vue
+++ src/views/article.vue
@@ -126,6 +126,11 @@ export default {
                             if (key === "effective") {
                                 data = this.getUnitNames(data)
                             }
+                            // change time format for long recruitment speed divisible on hours
+                            if(key === "recSpeed" && !(data % 3600)){
+                                console.log("change recSpeed")
+                                data = data / 3600 +"h";
+                            }
                         }
                         parsedText += data;
                     }
